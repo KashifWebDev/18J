b3fa0acc95f8e8eab8373245f130af2cb60a417b
Kaskhif Ali <kmalik748@gmail.com>
2022-02-01 22:28:42 +0500

    AfterReConsolation

--- quoteGeneratePDF.php
+++ quoteGeneratePDF.php
@@ -83,6 +83,7 @@ $path = 'C:\xampp\htdocs\18J\generatedPDFs\\'.$PDFfilename;
 $path = 'https://18jorissen.co.za/app/generatedPDFs/'.$PDFfilename;
 $path = '/public_html/app/generatedPDFs/'.$PDFfilename;
 //echo realpath($_SERVER["DOCUMENT_ROOT"]); exit(); die();
+echo $PDFfilename; exit(); die();
 $path = realpath($_SERVER["DOCUMENT_ROOT"]).$PDFfilename;
 //ob_clean();
 echo $pdf->Output($path, 'F');
