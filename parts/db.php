096ad0b7a6270b4114bbfbc4b76a343995b2d6bf
Kaskhif Ali <kmalik748@gmail.com>
2022-02-01 22:08:25 +0500

    AfterReConsolation

--- parts/db.php
+++ parts/db.php
@@ -1,6 +1,6 @@
 <?php
 
-//$con = mysqli_connect("localhost","root","","project_18j");
+$con = mysqli_connect("localhost","root","","project_18j");
 $con = mysqli_connect("localhost","j18jocnn_18joris","CPvvGapgy)Oy","j18jocnn_18jor");
 
 // Check connections
