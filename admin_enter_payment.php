3fdadd9d26ecefc8f8b383499f2be5abbdc9642c
Kaskhif Ali <kmalik748@gmail.com>
2022-02-19 11:55:06 +0500

    AfterReConsolation

--- admin_enter_payment.php
+++ admin_enter_payment.php
@@ -166,9 +166,9 @@ require 'parts/head.php';
                         $timestamp =  date('Y-m-d H:i:s', time());
 
                         $sql = "INSERT INTO invoice (userID, paymentDate, startDate, roomType,
-                                                    totalAmount, totalDays, date_time, reg, dep)
+                                                    totalAmount, date_time, reg, dep)
                                 VALUES ($userID, '$paymentDate', '$startDate', '$roomType',
-                                        '$totalAmountToPay','$days', '$timestamp', '$reg', '$dep')";
+                                        '$totalAmountToPay', '$timestamp', '$reg', '$dep')";
 
 
                         if(mysqli_query($con, $sql)){
